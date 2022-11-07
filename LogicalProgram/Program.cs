65b0ad4f573929653ea88a7b74bc4a956aef8129
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-07 02:04:43 -0800

    Added Fibonacci series pgrm

---
+++ LogicalProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LogicalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n1 = 0, n2 = 1, n3, i, num;
+            Console.Write("Enter the number of elements: ");
+            num = int.Parse(Console.ReadLine()); //taking input fron users
+            Console.Write(n1 + " " + n2 + " ");
+            for (i = 2; i < num; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
