97bbf608eb27e5df576b7af7d08996dc4888eef4
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-11-07 03:18:49 -0800

    Temperature Conversion  Program

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -1,4 +1,4 @@
-﻿// Vending Program
+﻿//Temperature Converter Program
 using System;
 using System.Diagnostics;
 using System.Linq;
@@ -6,116 +6,39 @@ using System.Linq;
 namespace LogicalProgram
 {
 
-    internal class VendingMachine
+    internal class Temperature
     {
-        public int Number;
-
-        public VendingMachine(int number)
+        public static int Value;
+        public Temperature(int value)
         {
-            Number = number;
+            Value = value;
         }
 
-        public void calculateChange()
+        public static void temperatureConversion()
         {
-            int thousand = 0;
-            int fiveHundred = 0;
-            int hundred = 0;
-            int fifty = 0;
-            int ten = 0;
-            int five = 0;
-            int two = 0;
-            int one = 0;
-            if (Number >= 1000)
-            {
-                while (Number >= 1000)
-                {
-                    Number = Number - 1000;
-                    thousand++;
-                }
-                Console.WriteLine("1000 Rupees Notes :" + thousand);
-            }
-            if (Number >= 500)
-            {
-                while (Number >= 500)
-                {
-                    Number = Number - 500;
-                    fiveHundred++;
-                }
-                Console.WriteLine("500 Rupees Notes :" + fiveHundred);
-            }
-            if (Number >= 100)
-            {
-                while (Number >= 100)
-                {
-                    Number = Number - 100;
-                    hundred++;
-                }
-                Console.WriteLine("100 Rupees Notes :" + hundred);
-            }
-            if (Number >= 50)
-            {
-                while (Number >= 50)
-                {
-                    Number = Number - 50;
-                    fifty++;
-                }
-                Console.WriteLine("50 Rupees Notes :" + fifty);
-            }
-            if (Number >= 10)
-            {
-                while (Number >= 10)
-                {
-                    Number = Number - 10;
-                    ten++;
-                }
-                Console.WriteLine("1000 Rupees Notes :" + ten);
-            }
-            if (Number >= 5)
-            {
-                while (Number >= 5)
-                {
-                    Number = Number - 5;
-                    five++;
-                }
-                Console.WriteLine("1000 Rupees Notes :" + five);
-            }
+            Console.WriteLine("Please Enter Value : ");
+            int value = Convert.ToInt32(Console.ReadLine());
 
-            if (Number >= 5)
+            switch (value)
             {
-                while (Number >= 5)
-                {
-                    Number = Number - 5;
-                    five++;
-                }
-                Console.WriteLine("1000 Rupees Notes :" + five);
-            }
-            if (Number >= 2)
-            {
-                while (Number >= 2)
-                {
-                    Number = Number - 2;
-                    two++;
-                }
-                Console.WriteLine("1000 Rupees Notes :" + two);
-            }
-            if (Number >= 1)
-            {
-                while (Number >= 1)
-                {
-                    Number = Number - 1;
-                    one++;
-                }
-                Console.WriteLine("1000 Rupees Notes :" + one);
+                case 1:
+                    Console.WriteLine("Please Enter the Value to Convert Tempearture into Celcius: ");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    int result1 = (a * 9 / 5) + 32;
+                    Console.WriteLine("Celcius Value is:" + result1);
+                    break;
+                case 2:
+                    Console.WriteLine("Please Enter the Value to Convert Tempearture into Fahrenheit: ");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    int result2 = (b - 32) * 5 / 9;
+                    Console.WriteLine("Fahrenheit Value is:" + result2);
+                    break;
+
             }
         }
-
-        static void Main(String[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Please Enter Number of Rupees  :");
-            int n = Convert.ToInt32(Console.ReadLine());
-
-            VendingMachine vendingMachine = new VendingMachine(n);
-            vendingMachine.calculateChange();
+            Temperature.temperatureConversion();
         }
     }
 }
